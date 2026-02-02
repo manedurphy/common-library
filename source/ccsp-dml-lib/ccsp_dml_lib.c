11b4bb680c1377913d715e05cf8796269fa25920
Dane Murphy <danem7@gmail.com>
2026-02-02 08:15:03 -0800

    WIP

--- source/ccsp-dml-lib/ccsp_dml_lib.c
+++ source/ccsp-dml-lib/ccsp_dml_lib.c
@@ -148,7 +148,8 @@ int ccsp_dml_init(const char *component_name, const char *json_file_path,
 
   FILE *file = fopen(json_file_path, "r");
   if (!file) {
-    CcspTraceError(("Cannot open JSON config file: %s\n", json_file_path));
+    CcspTraceError(("Cannot open JSON config file: path=%s err=%s\n",
+                    json_file_path, strerror(errno)));
     return -1;
   }
 
